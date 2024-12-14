17ff818168e79b8ffb938940fd85a42dc905f77d
Hollicint <hollyjdowling@gmail.com>
2024-12-14 15:52:04 +0000

    Added Repository Design Pattern

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,19 @@
 class BooksController < ApplicationController
+  # actions show,edit,update,delete
   before_action :set_book, only: %i[ show edit update destroy ]
+  # initializes the repository in one go without repeating throughout the page
+  before_action :set_repository
 
   # GET /books or /books.json
   def index
-    @books = Book.all
+
+    #Creating instance of repository
+    # @book_repository = BookRepository.new
+
+    # Uses repository to get books
+    @books = @book_repository.all
+        # @books = Book.all
+
   end
 
   # GET /books/1 or /books/1.json
@@ -21,7 +31,12 @@ class BooksController < ApplicationController
 
   # POST /books or /books.json
   def create
-    @book = Book.new(book_params)
+   # @book = Book.new(book_params)
+
+   # new repository instance
+   # @book_repository =BookRepository.new
+   # create new book with repository
+    @book = @book_repository.create(book_params)
 
     respond_to do |format|
       if @book.save
@@ -36,8 +51,13 @@ class BooksController < ApplicationController
 
   # PATCH/PUT /books/1 or /books/1.json
   def update
+    # new repository instance
+   # @book_repository =BookRepository.new
+
     respond_to do |format|
-      if @book.update(book_params)
+      #if @book.update(book_params)
+      # repository updates the book
+      if @book_repository.update(@book, book_params)
         format.html { redirect_to @book, notice: "Book was successfully updated." }
         format.json { render :show, status: :ok, location: @book }
       else
@@ -49,10 +69,15 @@ class BooksController < ApplicationController
 
   # DELETE /books/1 or /books/1.json
   def destroy
-    @book.destroy!
+   # @book.destroy!
+
+    # new repository instance
+    # @book_repository =BookRepository.new
+    # delete book with repository
+    @book_repository.delete(@book)
 
     respond_to do |format|
-      format.html { redirect_to books_path, status: :see_other, notice: "Book was successfully destroyed." }
+      format.html { redirect_to books_path, status: :see_other, notice: "Book was successfully Deleted." }
       format.json { head :no_content }
     end
   end
@@ -60,8 +85,15 @@ class BooksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
-      @book = Book.find(params.expect(:id))
+      #@book = Book.find(params.expect(:id))
+      #located book by id
+      @book = Book.find(params[:id])
     end
+
+    #initializes the repository at the before_action line at the top of the page
+    def set_repository
+      @book_repository = BookRepository.new 
+    end  
 
     # Only allow a list of trusted parameters through.
     def book_params
---
+++ app/repository/book_repository.rb
@@ -0,0 +1,31 @@
+## Repository Template
+
+class BookRepository
+
+    #Gets all the books from database
+    def all
+        Book.all
+    end
+    
+    #Locates book by certain Id
+    def find
+        Book.find(id)
+    end    
+
+    # Creates the new book with a parameter
+    def create(book_param)
+        Book.create(book_param)
+    end
+    
+    # Updates the book with the parameter
+    def update(book, book_param)
+        book.update(book_param)
+    end
+    
+    # Deletes the book
+    def delete(book)
+        book.destory
+    end    
+
+    
+end    
